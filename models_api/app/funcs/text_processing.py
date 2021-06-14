7d268272ba268d790e7ae60d1571aa77944f1542
YiLiu6240 <yi.liu6240@gmail.com>
2021-06-14 08:40:20 +0100

    Correct wording

--- models_api/app/funcs/text_processing.py
+++ models_api/app/funcs/text_processing.py
@@ -2,14 +2,14 @@ import spacy
 
 
 def preprocess_encode(text: str, nlp_model: spacy.language.Language) -> str:
-    text = lower_capitalised(text)
+    text = lower_titled(text)
     # use the lemmatised tokens as text
     doc = nlp_model(text)
     clean_text = " ".join([token.lemma_ for token in doc])
     return clean_text
 
 
-def lower_capitalised(text: str) -> str:
+def lower_titled(text: str) -> str:
     """
     'Body mass index' -> 'body mass index',
     'FEVER' -> 'FEVER'
